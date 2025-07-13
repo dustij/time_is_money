158cf6fdca6460e16e94f7b7fb8ce3ea781da548
dustij <dev.dusti@gmail.com>
2025-07-13 09:36:15 -0500

    i think im back

---
+++ lib/core/services/file_storage.dart
@@ -0,0 +1,50 @@
+import "dart:io";
+
+import "package:path_provider/path_provider.dart";
+
+class FileStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localHistoryFile async {
+    final path = await _localPath;
+    return File("$path/history.txt");
+  }
+
+  Future<File> get _localHourlyRateFile async {
+    final path = await _localPath;
+    return File("$path/rate.txt");
+  }
+
+  Future<File> appendHistory(String text) async {
+    final file = await _localHistoryFile;
+    return file.writeAsString("$text\n", mode: FileMode.append, flush: true);
+  }
+
+  Future<String> readHistory() async {
+    try {
+      final file = await _localHistoryFile;
+      final contents = await file.readAsString();
+      return contents;
+    } catch (e) {
+      return "";
+    }
+  }
+
+  Future<File> writeHourlyRate(double value) async {
+    final file = await _localHourlyRateFile;
+    return file.writeAsString("$value\n");
+  }
+
+  Future<double> readHourlyRate() async {
+    try {
+      final file = await _localHourlyRateFile;
+      final contents = await file.readAsString();
+      return double.parse(contents);
+    } catch (e) {
+      return 0;
+    }
+  }
+}
